ca60b6e2997329186b25872fd5f6c62a58ecc1a1
burmist <burm.leonid@gmail.com>
2022-10-01 00:09:59 +0200

    Update

--- nm.bash
+++ nm.bash
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-########################################################################
-#                                                                      #
-# Copyright(C) 2017 - LBS - (Single person developer.)                 #
-# Thu Oct 26 16:24:03 JST 2017                                         #
-# Autor: Leonid Burmistrov                                             #
-#                                                                      #
-# File description:                                                    #
-#                  This script prints list the functions in a shared   #
-#                  library.                                            #
-#                                                                      #
-# Input paramete:                                                      #
-#                  NON.                                                #
-#                                                                      #
-# This software is provided "as is" without any warranty.              #
-#                                                                      #
-########################################################################
-
 function nm_bash {
 
     liblist=$1
